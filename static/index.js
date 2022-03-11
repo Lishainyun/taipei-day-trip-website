d2985243be120520581ae2675fd3ea6cc5f5d483
Yun <chengyunlee1995@gmail.com>
2022-03-11 20:58:55 +0800

    test1

---
+++ static/index.js
@@ -0,0 +1,65 @@
+"use strict"
+
+// let searchInput = document.getElementById('searchInput').value
+// let searchClick = document.getElementById('searchBtnBackground')
+// let searchEnter = document.getElementById('searchInput').value
+
+(function loadData(){
+
+    let url = 'http://44.199.90.64:3000/api/attractions?'
+    let page = 0
+    // let keyword = searchInput
+    // '&keyword=' + keyword
+    fetch(url + 'page=' + page, {
+        method:'get'
+    })
+    .then(response => response.json())
+    .then((res) => {
+
+        let data = res['data']
+        let dataLength = data.length
+        // let mrt = data['data']['mrt']
+        // let category = data['data']['category']
+        // let nextPage = data['nextPage']
+        for(let i = 0; i < dataLength; i++){
+
+            let attractionsPic = data[i]['images'][0];
+            let mrt = data[i]['mrt'];
+            let category = data[i]['category'];
+
+            let attractionsContainer = document.getElementById('attractionsContainer');
+            let picDivTag = document.createElement('div');
+            let picImgTag = document.createElement('img');
+            let namePTag = document.createElement('p');
+            let mrtPTag = document.createElement('p');
+            let catPTag = document.createElement('p');
+            
+            let nameTextnode = document.createTextNode(data[i]['name']);
+            let mrtTextnode = document.createTextNode(mrt);
+            let catTextnode = document.createTextNode(category);
+
+            picDivTag.setAttribute('style', 'width:100%');
+            picImgTag.setAttribute('src',attractionsPic)
+            picImgTag.setAttribute('title', nameTextnode);
+            picImgTag.setAttribute('style','width:100%;aspect-ratio:16/9');
+            namePTag.setAttribute('style','color:#757575;font-weight:700;font-size:16px;line-height:13.3px;padding:10px 7px 10px 10px;margin:0');
+            mrtPTag.setAttribute('style','color:#757575;font-weight:400;font-size:16px;line-height:13.3px;width:50%;height:40px;margin:0');
+            catPTag.setAttribute('style','display:inline-block;color:#757575;font-weight:400;font-size:16px;line-height:13.3px;width:50%;height:40px;margin:0;text-align:right');
+            
+            attractionsContainer.appendChild(picDivTag);
+            picDivTag.appendChild(picImgTag);
+            picDivTag.appendChild(namePTag);
+            namePTag.appendChild(nameTextnode);
+            picDivTag.appendChild(mrtPTag);
+            mrtPTag.appendChild(mrtTextnode);
+            picDivTag.appendChild(catPTag);
+            catPTag.appendChild(catTextnode);
+        }
+    })
+}())
+
+searchClick.addEventListener("click", () => {
+    
+    
+
+})
