40fa84f6d7cb7b186cb925ac7f35b8bc301a4f2f
Yun <chengyunlee1995@gmail.com>
2022-03-12 21:29:47 +0800

    test

--- static/scroll.js
+++ static/scroll.js
@@ -2,6 +2,9 @@
 
 let ticking = false;
 
+let page = 0;
+let searchInput;
+
 function scroll(){
     
     let footer = document.getElementById('footer');
@@ -10,8 +13,7 @@ function scroll(){
     let footerBottom = footerRect.bottom;
     let footerHeight = footerRect.height;
     let url = 'http://44.199.90.64:3000/api/attractions?';
-    let page = 0;
-    let searchInput = ""
+    
 
     if(document.getElementById('searchInput').value === null){
         searchInput = ""
@@ -21,25 +23,67 @@ function scroll(){
 
     while( (footerBottom - footerTop) === footerHeight ) {
 
-        fetch(url + 'page=' + page + '&keyword=' + searchInput, {
+        fetch(url + 'page=' + page, {
             method:'get'
         })
         .then(response => response.json())
         .then((res) => {
 
-            if ( (!res['error']) && (res['nextPage'] !== null) ) {
+            while(res['nextPage'] !== null) {
 
                 page = res['nextPage']
 
-            } else {
-
-                page = 0
-
-            }
+            } 
         })
     }
     
-    while (page !== 0){
+    if (page !== 0 && searchInput === ""){
+
+        fetch(url + 'page=' + page , {
+            method:'get'
+        })
+        .then(response => response.json())
+        .then((response) => {
+            let data = response['data']
+            let dataLength = data.length
+
+            for(let i = 0; i < dataLength; i++){
+
+                let attractionsPic = data[i]['images'][0];
+                let mrt = data[i]['mrt'];
+                let category = data[i]['category'];
+        
+                let attractionsContainer = document.getElementById('attractionsContainer');
+                let picDivTag = document.createElement('div');
+                let picImgTag = document.createElement('img');
+                let namePTag = document.createElement('p');
+                let mrtPTag = document.createElement('p');
+                let catPTag = document.createElement('p');
+                        
+                let nameTextnode = document.createTextNode(data[i]['name']);        
+                let mrtTextnode = document.createTextNode(mrt);
+                let catTextnode = document.createTextNode(category);
+                
+                picDivTag.setAttribute('style', 'width:100%;border:1px solid #E8E8E8;border-radius:5px;overflow:hidden');
+                picImgTag.setAttribute('src',attractionsPic)
+                picImgTag.setAttribute('title', nameTextnode);
+                picImgTag.setAttribute('style','width:100%;aspect-ratio:16/9');
+                namePTag.setAttribute('style','color:#757575;font-weight:700;font-size:16px;line-height:13.3px;padding:10px 7px 10px 10px;margin:0;overflow:hidden;');
+                mrtPTag.setAttribute('style','color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;display:inline-block;padding-left:10px');
+                catPTag.setAttribute('style','display:inline-block;color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;text-align:right;float:right;padding-right:10px');
+                    
+                attractionsContainer.appendChild(picDivTag);
+                picDivTag.appendChild(picImgTag);
+                picDivTag.appendChild(namePTag);
+                namePTag.appendChild(nameTextnode);
+                picDivTag.appendChild(mrtPTag);
+                mrtPTag.appendChild(mrtTextnode);
+                picDivTag.appendChild(catPTag);
+                catPTag.appendChild(catTextnode);
+                footer.style.position = "relative";
+            }
+        })
+    } else if (page !== 0 && searchInput !== "") {
 
         fetch(url + 'page=' + page + '&keyword=' + searchInput, {
             method:'get'
@@ -50,6 +94,7 @@ function scroll(){
             let dataLength = data.length
 
             for(let i = 0; i < dataLength; i++){
+
                 let attractionsPic = data[i]['images'][0];
                 let mrt = data[i]['mrt'];
                 let category = data[i]['category'];
@@ -87,8 +132,8 @@ function scroll(){
     }
 }
 
-document.addEventListener('scroll', () => {
-    
+Document.addEventListener('scroll', () => {
+
     if (!ticking){
         window.requestAnimationFrame(() => {
             scroll();
