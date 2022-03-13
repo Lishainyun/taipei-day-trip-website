57af61fda3312c613f328de0920236afe84d3a46
Yun <chengyunlee1995@gmail.com>
2022-03-13 22:17:01 +0800

    final

--- static/indexOnload.js
+++ static/indexOnload.js
@@ -5,6 +5,20 @@ let nextPage = "";
 let searchInput = "";
 let enterSearch = document.getElementById('searchBtn');
 
+const debounce = function(func, delay){
+    let timer;
+    return function(){     
+        const context = this; 
+        const args = arguments;
+
+        clearTimeout(timer); 
+
+        timer = setTimeout(()=> {
+        func.apply(context, args)
+        },delay);
+    }
+}
+
 function onload(){
 
     let page = 0;
