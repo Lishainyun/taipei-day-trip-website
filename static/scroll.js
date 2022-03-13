74ff3b5131500eaa0a5471cf9cbeae35571db147
Yun <chengyunlee1995@gmail.com>
2022-03-13 15:03:09 +0800

    test

--- static/scroll.js
+++ static/scroll.js
@@ -1,5 +1,102 @@
 "use strict"
 
+function nextPageWithoutKeyword(){
+
+    fetch(url + 'page=' + page , {
+        method:'get'
+    })
+    .then(response => response.json())
+    .then((response) => {
+        let data = response['data']
+        let dataLength = data.length
+    
+        for(let i = 0; i < dataLength; i++){
+    
+            let attractionsPic = data[i]['images'][0];
+            let mrt = data[i]['mrt'];
+            let category = data[i]['category'];
+    
+            let attractionsContainer = document.getElementById('attractionsContainer');
+            let picDivTag = document.createElement('div');
+            let picImgTag = document.createElement('img');
+            let namePTag = document.createElement('p');
+            let mrtPTag = document.createElement('p');
+            let catPTag = document.createElement('p');
+                    
+            let nameTextnode = document.createTextNode(data[i]['name']);        
+            let mrtTextnode = document.createTextNode(mrt);
+            let catTextnode = document.createTextNode(category);
+            
+            picDivTag.setAttribute('style', 'width:100%;border:1px solid #E8E8E8;border-radius:5px;overflow:hidden');
+            picImgTag.setAttribute('src',attractionsPic)
+            picImgTag.setAttribute('title', nameTextnode);
+            picImgTag.setAttribute('style','width:100%;aspect-ratio:16/9');
+            namePTag.setAttribute('style','color:#757575;font-weight:700;font-size:16px;line-height:13.3px;padding:10px 7px 10px 10px;margin:0;overflow:hidden;');
+            mrtPTag.setAttribute('style','color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;display:inline-block;padding-left:10px');
+            catPTag.setAttribute('style','display:inline-block;color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;text-align:right;float:right;padding-right:10px');
+                
+            attractionsContainer.appendChild(picDivTag);
+            picDivTag.appendChild(picImgTag);
+            picDivTag.appendChild(namePTag);
+            namePTag.appendChild(nameTextnode);
+            picDivTag.appendChild(mrtPTag);
+            mrtPTag.appendChild(mrtTextnode);
+            picDivTag.appendChild(catPTag);
+            catPTag.appendChild(catTextnode);
+    
+        }
+    })
+
+}
+
+function nextPageWithKeyword(){
+
+    fetch(url + 'page=' + page + '&keyword=' + searchInput, {
+        method:'get'
+    })
+    .then(response => response.json())
+    .then((response) => {
+        let data = response['data']
+        let dataLength = data.length
+    
+        for(let i = 0; i < dataLength; i++){
+    
+            let attractionsPic = data[i]['images'][0];
+            let mrt = data[i]['mrt'];
+            let category = data[i]['category'];
+    
+            let attractionsContainer = document.getElementById('attractionsContainer');
+            let picDivTag = document.createElement('div');
+            let picImgTag = document.createElement('img');
+            let namePTag = document.createElement('p');
+            let mrtPTag = document.createElement('p');
+            let catPTag = document.createElement('p');
+                    
+            let nameTextnode = document.createTextNode(data[i]['name']);        
+            let mrtTextnode = document.createTextNode(mrt);
+            let catTextnode = document.createTextNode(category);
+            
+            picDivTag.setAttribute('style', 'width:100%;border:1px solid #E8E8E8;border-radius:5px;overflow:hidden');
+            picImgTag.setAttribute('src',attractionsPic)
+            picImgTag.setAttribute('title', nameTextnode);
+            picImgTag.setAttribute('style','width:100%;aspect-ratio:16/9');
+            namePTag.setAttribute('style','color:#757575;font-weight:700;font-size:16px;line-height:13.3px;padding:10px 7px 10px 10px;margin:0;overflow:hidden;');
+            mrtPTag.setAttribute('style','color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;display:inline-block;padding-left:10px');
+            catPTag.setAttribute('style','display:inline-block;color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;text-align:right;float:right;padding-right:10px');
+                
+            attractionsContainer.appendChild(picDivTag);
+            picDivTag.appendChild(picImgTag);
+            picDivTag.appendChild(namePTag);
+            namePTag.appendChild(nameTextnode);
+            picDivTag.appendChild(mrtPTag);
+            mrtPTag.appendChild(mrtTextnode);
+            picDivTag.appendChild(catPTag);
+            catPTag.appendChild(catTextnode);
+    
+        }
+    })
+
+}
 
 function scroll(){
     
@@ -19,7 +116,7 @@ function scroll(){
         searchInput = document.getElementById('searchInput').value
     }
 
-    while( (footerBottom - footerTop) === footerHeight ) {
+    if( (footerBottom - footerTop) === footerHeight && searchInput === "" ) {
 
         fetch(url + 'page=' + page, {
             method:'get'
@@ -29,105 +126,24 @@ function scroll(){
 
             while(res['nextPage'] !== null) {
 
-                page = res['nextPage']
-
-            } 
+                page = res['nextPage'];
+                nextPageWithoutKeyword()
+            }  
         })
-    }
-    
-    if (page !== 0 && searchInput === ""){
-
-        fetch(url + 'page=' + page , {
-            method:'get'
-        })
-        .then(response => response.json())
-        .then((response) => {
-            let data = response['data']
-            let dataLength = data.length
-
-            for(let i = 0; i < dataLength; i++){
-
-                let attractionsPic = data[i]['images'][0];
-                let mrt = data[i]['mrt'];
-                let category = data[i]['category'];
-        
-                let attractionsContainer = document.getElementById('attractionsContainer');
-                let picDivTag = document.createElement('div');
-                let picImgTag = document.createElement('img');
-                let namePTag = document.createElement('p');
-                let mrtPTag = document.createElement('p');
-                let catPTag = document.createElement('p');
-                        
-                let nameTextnode = document.createTextNode(data[i]['name']);        
-                let mrtTextnode = document.createTextNode(mrt);
-                let catTextnode = document.createTextNode(category);
-                
-                picDivTag.setAttribute('style', 'width:100%;border:1px solid #E8E8E8;border-radius:5px;overflow:hidden');
-                picImgTag.setAttribute('src',attractionsPic)
-                picImgTag.setAttribute('title', nameTextnode);
-                picImgTag.setAttribute('style','width:100%;aspect-ratio:16/9');
-                namePTag.setAttribute('style','color:#757575;font-weight:700;font-size:16px;line-height:13.3px;padding:10px 7px 10px 10px;margin:0;overflow:hidden;');
-                mrtPTag.setAttribute('style','color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;display:inline-block;padding-left:10px');
-                catPTag.setAttribute('style','display:inline-block;color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;text-align:right;float:right;padding-right:10px');
-                    
-                attractionsContainer.appendChild(picDivTag);
-                picDivTag.appendChild(picImgTag);
-                picDivTag.appendChild(namePTag);
-                namePTag.appendChild(nameTextnode);
-                picDivTag.appendChild(mrtPTag);
-                mrtPTag.appendChild(mrtTextnode);
-                picDivTag.appendChild(catPTag);
-                catPTag.appendChild(catTextnode);
-
-            }
-        })
-    } else if (page !== 0 && searchInput !== "") {
+    } else if ( (footerBottom - footerTop) === footerHeight && searchInput !== "" ) {
 
         fetch(url + 'page=' + page + '&keyword=' + searchInput, {
             method:'get'
         })
         .then(response => response.json())
         .then((response) => {
-            let data = response['data']
-            let dataLength = data.length
+            while(response['nextPage'] !== null) {
 
-            for(let i = 0; i < dataLength; i++){
-
-                let attractionsPic = data[i]['images'][0];
-                let mrt = data[i]['mrt'];
-                let category = data[i]['category'];
-        
-                let attractionsContainer = document.getElementById('attractionsContainer');
-                let picDivTag = document.createElement('div');
-                let picImgTag = document.createElement('img');
-                let namePTag = document.createElement('p');
-                let mrtPTag = document.createElement('p');
-                let catPTag = document.createElement('p');
-                        
-                let nameTextnode = document.createTextNode(data[i]['name']);        
-                let mrtTextnode = document.createTextNode(mrt);
-                let catTextnode = document.createTextNode(category);
-                
-                picDivTag.setAttribute('style', 'width:100%;border:1px solid #E8E8E8;border-radius:5px;overflow:hidden');
-                picImgTag.setAttribute('src',attractionsPic)
-                picImgTag.setAttribute('title', nameTextnode);
-                picImgTag.setAttribute('style','width:100%;aspect-ratio:16/9');
-                namePTag.setAttribute('style','color:#757575;font-weight:700;font-size:16px;line-height:13.3px;padding:10px 7px 10px 10px;margin:0;overflow:hidden;');
-                mrtPTag.setAttribute('style','color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;display:inline-block;padding-left:10px');
-                catPTag.setAttribute('style','display:inline-block;color:#757575;font-weight:400;font-size:16px;line-height:40px;width:50%;height:40px;margin:0;text-align:right;float:right;padding-right:10px');
-                    
-                attractionsContainer.appendChild(picDivTag);
-                picDivTag.appendChild(picImgTag);
-                picDivTag.appendChild(namePTag);
-                namePTag.appendChild(nameTextnode);
-                picDivTag.appendChild(mrtPTag);
-                mrtPTag.appendChild(mrtTextnode);
-                picDivTag.appendChild(catPTag);
-                catPTag.appendChild(catTextnode);
-
-            }
+                page = res['nextPage'];
+                nextPageWithoutKeyword()
+            }  
         })
-    }
+    }  
 }
 
 window.addEventListener('scroll', scroll);
