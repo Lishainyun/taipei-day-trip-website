a53b5099893cf117ddb635dacddacf17eb03657d
Yun <chengyunlee1995@gmail.com>
2022-04-15 23:46:35 +0800

    fix loader_32

--- static/indexScroll.js
+++ static/indexScroll.js
@@ -67,7 +67,7 @@ function scroll(){
             }
             // loader
             let loader = document.getElementById('loader')
-            // attractionsContainer.removeChild(loader)
+            attractionsContainer.removeChild(loader)
             
             for (let i = 0; i < attractionsContainer.children.length; i++){
                 if (attractionsContainer.children[i].tagName == "DIV"){
