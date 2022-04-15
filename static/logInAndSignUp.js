91c1acc2b7279341192ce874f265167892b87332
Yun <chengyunlee1995@gmail.com>
2022-04-16 00:58:19 +0800

    fix navbar

--- static/logInAndSignUp.js
+++ static/logInAndSignUp.js
@@ -23,7 +23,7 @@ let userId;
 
 // 載入頁面確認登入狀態
 async function checkUserStatus(userApiUrl){
-    document.querySelector('.nav').style.display = 'block'
+    document.querySelector('.nav').style.display = 'none'
     const response = await fetch(userApiUrl,{
         method:"GET"
     })
