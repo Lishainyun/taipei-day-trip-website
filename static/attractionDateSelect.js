d48e721b2d62f067954b366d8f05600c4f353c8e
Yun <chengyunlee1995@gmail.com>
2022-04-17 20:39:54 +0800

    fix bookingBtn disabled issue

--- static/attractionDateSelect.js
+++ static/attractionDateSelect.js
@@ -4,7 +4,6 @@ bookingDate.addEventListener('input',()=>{
   if (bookingDate.checkValidity()){
     bookingDate.classList.remove('has-notSelected');
     bookingDate.classList.add('has-selected');
-    bookingBtn.disabled = false;
   } 
 })
 
