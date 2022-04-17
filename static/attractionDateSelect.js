644922b56880db9af8f3b0c8a441a3a8e22163ca
Yun <chengyunlee1995@gmail.com>
2022-04-17 20:05:02 +0800

    attraction page date validation feature

---
+++ static/attractionDateSelect.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let bookingDate = document.querySelector('.bookingDate')
+
+bookingDate.addEventListener('input',()=>{
+  if (bookingDate.checkValidity()){
+    bookingDate.classList.remove('has-notSelected');
+    bookingDate.classList.add('has-selected');
+    bookingBtn.disabled = false;
+  } else {
+    bookingDate.classList.remove('has-selected');
+    bookingDate.classList.add('has-notSelected');
+    bookingBtn.disabled = true;
+  }
+})
+
+
